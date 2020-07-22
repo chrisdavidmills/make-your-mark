6b150f78dd64871864426ac6d2c2a5d6077fbdd6
Chris Mills <cmills@mozilla.com>
2020-07-22 07:53:38 +0100

    updating payment select to simple show/hide

--- script/payment-select.js
+++ script/payment-select.js
@@ -3,35 +3,20 @@
 const cardRadio = document.querySelector('input[value="card"]');
 const paypalRadio = document.querySelector('input[value="paypal"]');
 
-const paymentTitle = document.querySelector('.payment-title');
-const paymentPara = document.querySelector('.payment-para');
-const paymentBtn = document.querySelector('.payment-button');
+const paymentCredit = document.querySelector('.payment-credit');
+const paymentPaypal = document.querySelector('.payment-paypal');
 
-const textStrings = [
-  {
-    title: 'Credit/debit card payment',
-    paragraph: 'You have the "Credit/debit card" payment option selected. Press the button below to be taken to the website of our trusted payment vendor — Stripe — where you can fill in your card details to pay for your order. Once you have completed payment, you will be redirected back to this website.',
-    button: 'Proceed to Stripe website'
-  },
-  {
-    title: 'Paypal payment',
-    paragraph: 'You have the "Paypal" payment option selected. Press the button below to be taken to the Paypal website, where you can confirm your details and pay for your order. Once you have completed payment, you will be redirected back to this website.',
-    button: 'Proceed to Paypal website'
-  }
-]
+// show appropriate information depending on radio button selection
 
-// show appropriate information depending on radio button
-
-function updatePaymentInfo(index) {
-  paymentTitle.textContent = textStrings[index].title;
-  paymentPara.textContent = textStrings[index].paragraph;
-  paymentBtn.textContent = textStrings[index].button;
-}
+paymentCredit.style.display = 'block';
+paymentPaypal.style.display = 'none';
 
 cardRadio.addEventListener('change', function() {
-  updatePaymentInfo(0);
+  paymentCredit.style.display = 'block';
+  paymentPaypal.style.display = 'none';
 })
 
 paypalRadio.addEventListener('change', function() {
-  updatePaymentInfo(1);
+  paymentCredit.style.display = 'none';
+  paymentPaypal.style.display = 'block';
 })
